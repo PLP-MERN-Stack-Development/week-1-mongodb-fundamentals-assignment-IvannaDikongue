6f94f363b6d4e2f2c7595e08910043458287f848
Ivanna <ivannadikongue@gmail.com>
2025-06-17 20:53:57 +0200

    Initial Commit

---
+++ insert_books_.js
@@ -0,0 +1,14 @@
+use plp_bookstore
+db.createCollection('books')
+
+{
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  genre: "Classic",
+  published_year: 1925,
+  price: 10.99,
+  in_stock: true,
+  pages: 180,
+  publisher: "Scribner"
+}
+
